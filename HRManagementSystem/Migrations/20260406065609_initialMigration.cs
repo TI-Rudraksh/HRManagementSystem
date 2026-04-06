ac68962460204c4253dc2c1e941a21fc3be8315e
Dhavalp-cyber <dhavalpatel49150@gmail.com>
2026-04-06 12:28:40 +0530

    Add first Migration

---
+++ HRManagementSystem/Migrations/20260406065609_initialMigration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Dept_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Dept_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Emp_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Emp_Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Emp_Age = table.Column<int>(type: "int", nullable: false),
+                    Emp_Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Emp_Mobile = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Emp_Salary = table.Column<int>(type: "int", nullable: false),
+                    Emp_Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Emp_Status = table.Column<bool>(type: "bit", nullable: false),
+                    Emp_Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dept_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Emp_Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_Dept_Id",
+                        column: x => x.Dept_Id,
+                        principalTable: "Departments",
+                        principalColumn: "Dept_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Dept_Id",
+                table: "Employees",
+                column: "Dept_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
